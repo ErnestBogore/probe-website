d03725d25eefca38a3a6db957d72ea5363d6f1d0
ErnestBogore <bogorernest@gmail.com>
2025-08-17 17:16:35 +0200

    feat(nav): update dropdown styles

--- src/components/marketing/client-nav.tsx
+++ src/components/marketing/client-nav.tsx
@@ -33,13 +33,13 @@ const ListItem = React.forwardRef<
         <a
           ref={ref}
           className={cn(
-            "block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground",
-            className,
+            "group block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-[#8858ed] focus:bg-accent focus:text-accent-foreground",
+            className
           )}
           {...props}
         >
-          <div className="text-sm font-medium leading-none">{title}</div>
-          <p className="line-clamp-2 text-sm leading-snug text-muted-foreground">
+          <div className="text-sm font-bold leading-none group-hover:text-white">{title}</div>
+          <p className="line-clamp-2 text-sm leading-snug text-muted-foreground group-hover:text-gray-200">
             {children}
           </p>
         </a>
