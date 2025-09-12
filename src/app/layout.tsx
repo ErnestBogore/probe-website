46d3e82477255b66d455153a1e2b093d37c99169
ErnestBogore <bogorernest@gmail.com>
2025-09-13 00:05:17 +0200

    Add Simple Analytics script for privacy-first analytics tracking

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,6 +26,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        {/* Simple Analytics - 100% privacy-first analytics */}
+        <script async src="https://scripts.simpleanalyticscdn.com/latest.js"></script>
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
